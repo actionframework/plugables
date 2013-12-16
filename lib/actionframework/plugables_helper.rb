b60c52a92c5e010012dc7962d7c9d6d0f5cf7462
bvandenbogaerde <bram.vandenbogaerde@gmail.com>
2013-12-16 20:55:55 +0100

    Code to load plugables [init]

--- lib/actionframework/plugables_helper.rb
+++ lib/actionframework/plugables_helper.rb
@@ -1,5 +1,13 @@
 module ActionFramework
 	class PlugablesHelper
-		
+		def self.load plugable
+			if File.exist? Dir.home+"/.actionframework/plugs/"+plugable+".rb" || File.exist? Dir.home+"/.actionframework/plugables/plugs/"+plugable+".rb"
+				require File.join(Dir.home,".actionframework/plugs/"+plugable)
+			elsif File.exist? Dir.home+"/.actionframework/plugables/Plugsinfo/"+plugable+".rb"
+				puts "Plugable# #{plugable} in not installed, run $ action install"
+			else
+				puts "Plugable #{plugable} doens't exist"
+			end	
+		end
 	end
 end
